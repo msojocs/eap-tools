75c8a7d997c1bc191a21c0cbcce0e342f6d4a070
msojocs <jiyecafe@gmail.com>
2022-08-12 22:34:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setups
+</script>
+
+<template>
+  <div class="layout">
+    <el-menu default-active="2" class="el-menu-vertical-demo" background-color="#545c64">
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon>
+            <location />
+          </el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group>
+          <template #title><span>Group One</span></template>
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title><span>item four</span></template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon>
+          <icon-menu />
+        </el-icon>
+        <template #title>Navigator Two</template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon>
+          <document />
+        </el-icon>
+        <template #title>Navigator Three</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon>
+          <setting />
+        </el-icon>
+        <template #title>Navigator Four</template>
+      </el-menu-item>
+    </el-menu>
+    <div>
+      <p>
+        <!--使用 router-link 组件进行导航 -->
+        <!--通过传递 `to` 来指定链接 -->
+        <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/about">Go to About</router-link>
+      </p>
+      <!-- 路由出口 -->
+      <!-- 路由匹配到的组件将渲染在这里 -->
+      <router-view></router-view>
+    </div>
+    <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div> -->
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './utils/routes'
+// 如果您正在使用CDN引入，请删除下面一行。
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app
+.use(router)
+.use(ElementPlus)
+.mount('#app')
